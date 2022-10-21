d98e07d6f837018480e9c34e0c6f677e19a0b4c6
Pravin Padhye <rpadhye5@gmail.com>
2022-10-21 08:11:11 +0000

    this is the latest and final commit of main branch

--- saturn1.py
+++ saturn1.py
@@ -5,13 +5,8 @@ tata steel
 sbi
 tata power
 tata motor
-axis
 ICICI
-Axis
 This is the public servent !!!!!!!!  newly created and edited branch
-echo
-echo
-
 there is a substutution of github and the latest version of the github, its all of the replacement of github
 echo
 echo
